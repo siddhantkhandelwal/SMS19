46a63dc219ce8daafee1e7fc594442558652d913
ashryaagr <ashryaagr@gmail.com>
2019-03-16 20:26:44 +0530

    Google OAuth integrated

--- main/views.py
+++ main/views.py
@@ -11,6 +11,13 @@ import re
 
 special_character_regex = re.compile(r'[@_!#$%^&*()<>?/\|}{~:]')
 
+# This function creates a UserProfile for a Social Account login
+def create_UserProfile(request, user):
+    if UserProfile.objects.filter(user=user).exists():
+        pass
+    else :
+        new_user = UserProfile(user=user, name = user.get_full_name())
+        new_user.save()
 
 def register(request):
     if request.user.is_authenticated:
@@ -114,4 +121,5 @@ def user_forgot_password(request):
 
 @login_required
 def game(request):
+    create_UserProfile(request, request.user)
     return HttpResponse("Giddy Up!")
