7bbd9d95d571ba64de95589d472e38649c878534
Siddhant Khandelwal <siddhantrkhandelwal@gmail.com>
2019-03-22 22:21:19 +0530

    Static Files

--- SMS19/settings.py
+++ SMS19/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_URL = 'user_login'
 
 EMAIL_USE_TLS = True
