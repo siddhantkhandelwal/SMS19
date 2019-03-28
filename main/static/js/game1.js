6e03f1851150743c1ad4e8e63a48c59d5cf417df
rohitrajhans <rohitrajhans07@gmail.com>
2019-03-28 08:39:53 +0530

    market status added in frontend

--- main/static/js/game1.js
+++ main/static/js/game1.js
@@ -22,6 +22,12 @@ function get_stock_list(code) {
             $('.loader').hide();
         },
         success: function (data) {
+
+            let marketStatus  = true;
+            if(!marketStatus) {
+                alert('This market has been temporarily closed by EFA');
+            }
+
             console.log(data);
             stock_list = data.stocks_list;
             // balance = data.balance;
@@ -100,6 +106,11 @@ function get_stock_list(code) {
                     var panel = this.nextElementSibling;
                     // console.log(panel);
                     // panel.style.maxHeight = panel.scrollHeight + "px";
+                    let stockStatus = true;
+                    if( !stockStatus) {
+                        alert('This stock has been closed by EFA.');
+                        return;
+                    }
                     if (panel.style.height) {
                         panel.style.height = null;
                     } else {
