be899ba69709658a65e0869521089507a30285a6
Siddhant Khandelwal <siddhantrkhandelwal@gmail.com>
2019-03-30 18:40:55 +0530

    Fixed Leaderboard

--- main/views.py
+++ main/views.py
@@ -532,8 +532,8 @@ def final_leaderboard_data(request):
                              'message': 'Error in Calculating Net Worth'}
             return JsonResponse(response_data)
         lb_data[user_profile.user.username] = user_profile.net_worth
-        sorted_lb_data = sorted(
-            lb_data.items(), key=operator.itemgetter(1), reverse=True)
+    sorted_lb_data = sorted(
+        lb_data.items(), key=operator.itemgetter(1), reverse=True)
     list_user_name = [x[0] for x in sorted_lb_data]
     list_net_worth = [x[1] for x in sorted_lb_data]
     count = len(list_net_worth)
@@ -564,8 +564,8 @@ def leaderboard_data(request):
                              'message': 'Error in Calculating Net Worth'}
             return JsonResponse(response_data)
         lb_data[user_profile.user.username] = user_profile.net_worth
-        sorted_lb_data = sorted(
-            lb_data.items(), key=operator.itemgetter(1), reverse=True)
+    sorted_lb_data = sorted(
+        lb_data.items(), key=operator.itemgetter(1), reverse=True)
     list_user_name = [x[0] for x in sorted_lb_data]
     try:
         current_user_rank = list_user_name.index(user_profile.user.username)
@@ -578,20 +578,6 @@ def leaderboard_data(request):
     count = len(list_net_worth)
     list_rank = [i for i in range(1, count+1)]
     user_profile = UserProfile.objects.get(user=request.user)
-
-    try:
-        user_profile.net_worth = 0
-        user_stocks_purchased = StockPurchased.objects.filter(
-            owner=user_profile)
-        for stock_purchased in user_stocks_purchased:
-            user_profile.net_worth += (stock_purchased.stock.stock_price) * \
-                (stock_purchased.units)
-        user_profile.net_worth += user_profile.balance
-        user_profile.save()
-    except:
-        response_data = {'status': 'error',
-                         'message': 'Error in Calculating Net Worth'}
-        return JsonResponse(response_data)
 
     response_data = {'list_rank': list_rank,
                      'list_user_name': list_user_name,
